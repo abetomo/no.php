9dd7ebdb407960694be7c6682bb0b5600abf5b17
Abe Tomoaki <abe@clear-code.com>
2024-08-30 13:59:08 +0900

    Pass REMOTE_USER to the backend
    We would like to pass `REMOTE_USER` to the backend and use it for authentication.
    
    `REMOTE_USER` is added because it was not included in the target to be passed.
    It is passed to the backend with the ` X-Forwarded-User` header.

--- no.php
+++ no.php
@@ -68,6 +68,8 @@ function getRequestHeaders($multipart_delimiter=NULL) {
                 // Handle application/json
                 array_push($headers, "$key: $value");
             }
+        } elseif ($key === 'REMOTE_USER') {
+            array_push($headers, "X-Forwarded-User: $value");
         }
     }
     return $headers;
