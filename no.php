30a491cdba78d888ac406884bd634ab289b71e61
Abe Tomoaki <abe@clear-code.com>
2025-10-31 08:48:02 +0900

    Set all values
    Since we made it so that the value is set with CONTENT_TYPE instead of HTTP_CONTENT_TYPE.

--- no.php
+++ no.php
@@ -71,17 +71,11 @@ function getRequestHeaders($multipart_delimiter=NULL) {
                     array_push($headers, "$key: $value");
             }
         } elseif (preg_match("/^CONTENT_TYPE/", $key)) {
-
             $key = "Content-Type";
-
             if(preg_match("/^multipart/", strtolower($value)) && $multipart_delimiter) {
                 $value = "multipart/form-data; boundary=" . $multipart_delimiter;
-                array_push($headers, "$key: $value");
             }
-            else if(preg_match("/^application\/json/", strtolower($value))) {
-                // Handle application/json
-                array_push($headers, "$key: $value");
-            }
+            array_push($headers, "$key: $value");
         } elseif ($key === 'REMOTE_USER') {
             array_push($headers, "X-Forwarded-User: $value");
         }
