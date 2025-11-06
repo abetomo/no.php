e1210e490a8a26d7a000fb7512e6c6d7cc7dbe2d
Abe Tomoaki <abe@clear-code.com>
2025-11-06 12:32:43 +0900

    Pass parameters to the backend as a hash
    PHP cannot distinguish between array and hash.
    Enable passing a hash with array indexes as keys to the backend.

--- no.php
+++ no.php
@@ -28,6 +28,7 @@ $backend_info = parse_url($backend_url);
 $host = $_SERVER['HTTP_HOST'];
 $request_uri = $_SERVER['REQUEST_URI'];
 $uri_rel = "subdir/no.php"; # URI to this file relative to public_html
+$params_hash_keys = [];
 
 $request_includes_nophp_uri = true;
 if ( $request_includes_nophp_uri == false) {
@@ -87,6 +88,8 @@ function build_domain_regex($hostname)
 }
 
 function build_multipart_data_fields($delimiter, $key, $value) {
+    global $params_hash_keys;
+
     $eol = "\r\n";
     if (!is_array($value)) {
         return "--" . $delimiter . $eol
@@ -95,7 +98,7 @@ function build_multipart_data_fields($delimiter, $key, $value) {
     }
 
     $data = '';
-    $is_array = array_key_first($value) === 0;
+    $is_array = array_key_first($value) === 0 && !in_array($key, $params_hash_keys);
     foreach ($value as $k => $v) {
         $data .= build_multipart_data_fields(
             $delimiter,
@@ -107,6 +110,8 @@ function build_multipart_data_fields($delimiter, $key, $value) {
 }
 
 function build_multipart_data_files($delimiter, $name, $value) {
+    global $params_hash_keys;
+
     if (!is_array($value)) {
         return '';
     }
@@ -120,7 +125,7 @@ function build_multipart_data_files($delimiter, $name, $value) {
     }
 
     $data = '';
-    $is_array = array_key_first($value) === 0;
+    $is_array = array_key_first($value) === 0 && !in_array($name, $params_hash_keys);
     foreach ($value as $k => $v) {
         $data .= build_multipart_data_files(
             $delimiter,
